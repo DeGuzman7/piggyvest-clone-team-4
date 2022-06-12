b79f89fe50e9bbb68311f00f9a79e502f3faae42
Ezeani Anthony Ikechukwu <ezeani.ikechukwu1995@gmail.com>
2022-06-12 16:25:22 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 const header = document.querySelector("#header");
+const openBtn = document.querySelector('#open');
+const closeBtn = document.querySelector('#close');
+
 window.addEventListener("scroll", () => {
   if (window.scrollY > 0) header.classList.add("scrollY");
   else header.classList.remove("scrollY");
 });
 
+openBtn.addEventListener("click", () => {
+  header.classList.add("clicked");
+});
+
+closeBtn.addEventListener("click", () => {
+  header.classList.remove("clicked");
+});
